73e4da493e9e5a6cf1164ec3035db3856b29ea87
glory-flory <sa.q9@yandex.ru>
2026-03-27 19:55:11 +0500

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("themeToggle");
+
+if (btn) {
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("light");
+
+        localStorage.setItem(
+            "theme",
+            document.body.classList.contains("light") ? "light" : "dark"
+        );
+    });
+}
+
+// загрузка темы
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light");
+}
